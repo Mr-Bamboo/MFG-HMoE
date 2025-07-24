784fd11dfbbc628e563b75b2270fe3968368df21
Bowen Chen <52306942+Mr-Bamboo@users.noreply.github.com>
2025-07-24 10:14:11 +0800

    Update __init__.py

--- hmoe/archs/__init__.py
+++ hmoe/archs/__init__.py
@@ -9,5 +9,5 @@ arch_folder = osp.dirname(osp.abspath(__file__))
 arch_filenames = [osp.splitext(osp.basename(v))[0] for v in scandir(arch_folder) if v.endswith('_arch.py')]
 # import all the arch modules
 # print(arch_filenames)
-_arch_modules = [importlib.import_module(f'hat.archs.{file_name}') for file_name in arch_filenames]
+_arch_modules = [importlib.import_module(f'hmoe.archs.{file_name}') for file_name in arch_filenames]
 
